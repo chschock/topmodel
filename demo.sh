8325559fcf4878b47e35b9d3d15d6629ef7225ec
Christoph Schock <chschock@gmail.com>
2018-12-11 22:53:12 +0100

    minor

--- demo.sh
+++ demo.sh
@@ -3,9 +3,9 @@ echo "start demo"
 for corpus in nips
 do
     echo "downloading UCI $corpus corpus"
-    wget http://archive.ics.uci.edu/ml/machine-learning-databases/bag-of-words/vocab.$corpus.txt
-    wget http://archive.ics.uci.edu/ml/machine-learning-databases/bag-of-words/docword.$corpus.txt.gz
-    gunzip docword.$corpus.txt.gz
+    wget -c http://archive.ics.uci.edu/ml/machine-learning-databases/bag-of-words/vocab.$corpus.txt
+    wget -c http://archive.ics.uci.edu/ml/machine-learning-databases/bag-of-words/docword.$corpus.txt.gz
+    gunzip -k docword.$corpus.txt.gz
     echo "preprocessing, translate from docword.txt to scipy format"
     python uci_to_scipy.py docword.$corpus.txt M_$corpus.full_docs.mat
     echo "preprocessing: removing rare words and stopwords"
